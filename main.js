faa7919d0237062272a1a6ce69cbdd6b7eb75209
LynxVito <mzb508@york.ac.uk>
2025-05-15 03:59:50 +0100

    started setting up stuff

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-// function setup()
-// {
-//     createCanvas(400, 400);
-// }
+function setup()
+{
+    createCanvas(400, 400);
+}
 
-// function draw()
-// {
-//     background(0, 50, 50);
-// }
+function draw()
+{
+    background(0, 50, 50);
+}
