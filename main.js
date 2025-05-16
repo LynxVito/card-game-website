4990b4b75a853cad00445f625663edb9e3483c31
LynxVito <mzb508@york.ac.uk>
2025-05-16 19:21:46 +0100

    blyek

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
+class Deck {
+    constructor() {
+        this.cards = [];
+        this.values = [2, 3, 4, 5, 6, 7, "Q", "J", "K", "A"];
+        this.suits = ["Spades", "Clubs", "Diamonds", "Hearts"];
+    }
+}
+
 function setup()
 {
-    createCanvas(400, 400);
+    createCanvas(600, 600);
+    rectMode(CENTER);
 }
 
 function draw()
 {
     background(0, 50, 50);
+}
+
+function createCard(x, y, s, v) {
+    return {
+        x: x, 
+        y: y,
+        suit: s,
+        value: v,
+
+    }
 }
