d10df37e5ea6ea7a175d3cbd7a95f0f464b1c3cc
LynxVito <mzb508@york.ac.uk>
2025-05-19 02:36:36 +0100

    you can now play and go fish

--- main.js
+++ main.js
@@ -4,23 +4,39 @@ let clubsPics = [];
 let diamondsPics = [];
 let heartsPics = [];
 
+//Stages
+const PRE_GAME = "Pre Game";
+const START = "Start";
+const DRAW_CARDS = "Draw Cards";
+const PLACE_CARDS = "Place Cards";
+const GO_FISH = "Go Fish";
+const COUNT_POINTS = "Count Points";
+const END_GAME = "End Game";
+
+let currentStage;
+
 let playerHand;
 let opponentHand;
 
 let cardHeight = 120;
 let cardWidth = 75;
 
+let trumpSuit;
+
 class Deck {
-    constructor() {
+    constructor(x, y) {
         this.cards = [];
-        this.values = ["A", 2, 3, 4, 5, 6, 7, "Q", "J", "K"];
+        this.values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
         this.suits = ["Spades", "Clubs", "Diamonds", "Hearts"];
+
+        this.x = x;
+        this.y = y;
     }
 
     sortDeck() {
         for (let s = 0; s < this.suits.length; s++) {
             for (let v = 0; v < this.values.length; v++) {
-                this.cards.push(createCard(0, 0, this.suits[s], this.values[v]));
+                this.cards.push(createCard(this.suits[s], this.values[v]));
             }
         }
     }
@@ -47,30 +63,85 @@ class Deck {
         }
     }
 
-    fish() {
-        this.cards.shift();
+    mouseOnDeck(px, py) {
+        if(px > this.x && px < this.x + cardWidth) {
+                if(py > this.y && py < this.y + cardHeight) {
+                    playerHand.cards.push(this.cards[0]);
+                    this.cards.shift();
+                }
+            }
+    }
+
+    draw() {
+        for(let i = 0; i < this.cards.length - 1; i++) {
+            image(backOfCardPic, this.x, this.y, cardWidth, cardHeight);
+        }
+
+        if(this.cards[this.cards.length - 1].suit == "Spades") {
+            image(spadesPics[this.cards[this.cards.length - 1].value - 1], this.x + cardWidth, this.y, cardWidth, cardHeight);
+        }
+        else if(this.cards[this.cards.length - 1].suit == "Clubs") {
+            image(clubsPics[this.cards[this.cards.length - 1].value - 1], this.x + cardWidth, this.y, cardWidth, cardHeight);
+        }
+        else if(this.cards[this.cards.length - 1].suit == "Diamonds") {
+            image(diamondsPics[this.cards[this.cards.length - 1].value - 1], this.x + cardWidth, this.y, cardWidth, cardHeight);
+        }
+        else if(this.cards[this.cards.length - 1].suit == "Hearts") {
+            image(heartsPics[this.cards[this.cards.length - 1].value - 1], this.x + cardWidth, this.y, cardWidth, cardHeight);
+        }
     }
 }
 
 class Hand {
-    constructor(opp = false) {
+    constructor(x, y, opp = false) {
         this.isOpponent = opp;
         this.cards = [];
+        this.x = x;
+        this.y = y;
+    }
+
+    mouseOnCard(px, py) {
+        for(let i = 0; i < this.cards.length; i++) {
+            if(px > this.x + cardWidth * i && px < this.x + cardWidth + cardWidth * i) {
+                if(py > this.y && py < this.y + cardHeight) {
+                    this.cards.splice(i, 1);
+                }
+            }
+        }     
+    }
+
+    draw() {
+        for(let i = 0; i < this.cards.length; i++) {
+            if(this.cards[i].suit == "Spades") {
+                image(spadesPics[this.cards[i].value - 1], this.x + cardWidth * i, this.y, cardWidth, cardHeight);
+            }
+            else if(this.cards[i].suit == "Clubs") {
+                image(clubsPics[this.cards[i].value - 1], this.x + cardWidth  * i, this.y, cardWidth, cardHeight);
+            }
+            else if(this.cards[i].suit == "Diamonds") {
+                image(diamondsPics[this.cards[i].value - 1], this.x + cardWidth  * i, this.y, cardWidth, cardHeight);
+            }
+            else if(this.cards[i].suit == "Hearts") {
+                image(heartsPics[this.cards[i].value - 1], this.x + cardWidth  * i, this.y, cardWidth, cardHeight);
+            }
+        }
     }
 }
 
 function preload() {
+    backOfCardPic = loadImage("Playing Cards/card-back1.png");
+
     for (let i = 1; i < 11; i++) {
-        let spadesPic = loadImage(`Playing Cards/card-spades-${i}.png`)
+        spadesPic = loadImage(`Playing Cards/card-spades-${i}.png`);
         spadesPics.push(spadesPic);
 
-        let clubsPic = loadImage(`Playing Cards/card-clubs-${i}.png`)
+        clubsPic = loadImage(`Playing Cards/card-clubs-${i}.png`);
         clubsPics.push(clubsPic);
 
-        let diamondsPic = loadImage(`Playing Cards/card-diamonds-${i}.png`)
+        diamondsPic = loadImage(`Playing Cards/card-diamonds-${i}.png`);
         diamondsPics.push(diamondsPic);
 
-        let heartsPic = loadImage(`Playing Cards/card-hearts-${i}.png`)
+        heartsPic = loadImage(`Playing Cards/card-hearts-${i}.png`);
         heartsPics.push(heartsPic);
     }
 }
@@ -78,36 +149,69 @@ function preload() {
 function setup() {
     createCanvas(600, 600);
     rectMode(CENTER);
+    angleMode(DEGREES);
+    imageMode(CORNER);
 
-    deck = new Deck();
+    deck = new Deck(0, height * 0.4);
     deck.sortDeck();
 
-    playerHand = new Hand(false);
-    opponentHand = new Hand(true);
-    // deck.shuffle(deck.cards);
+    playerHand = new Hand(width / 3, height - cardHeight, false);
+    opponentHand = new Hand(width / 3, 0, true);
+    deck.shuffle(deck.cards);
+
+    currentStage = START;
 }
 
 function draw() {
     background(0, 50, 50);
 
-    // image(spadesPics[0], cardWidth * 0, 0, cardWidth, cardHeight);
-    // image(diamondsPics[9], cardWidth * 1, 0, cardWidth, cardHeight);
+    console.log(currentStage);
+
+    if(currentStage == START) {
+        deck.dealCards();
+        trumpSuit = deck.cards[deck.cards.length - 1].suit;
+
+        console.log(trumpSuit);
+        console.log(deck.cards);
+        console.log(playerHand.cards);
+        console.log(opponentHand.cards);
+
+        playerHand.draw();
+        opponentHand.draw();
+        deck.draw();
+
+        currentStage = PLACE_CARDS;
+    }
+
+    else if(currentStage == PLACE_CARDS) {
+        playerHand.draw();
+        opponentHand.draw();
+        deck.draw();
+    }
+
+    else if(currentStage == GO_FISH) {
+        playerHand.draw();
+        opponentHand.draw();
+        deck.draw();
+    }
+        
 }
 
-function createCard(x, y, s, v) {
+function createCard(s, v) {
     return {
-        x: x, 
-        y: y,
         suit: s,
         value: v,
     }
 }
 
 function mousePressed() {
+    if(playerHand.cards.length > 2) {
+        playerHand.mouseOnCard(mouseX, mouseY)
+        // currentStage = GO_FISH;
+    }
+
     if(playerHand.cards.length < 3) {
-        deck.dealCards();
-        console.log(deck.cards)
-        console.log(playerHand.cards)
-        console.log(opponentHand.cards);
+        deck.mouseOnDeck(mouseX, mouseY);
+        // currentStage = PLACE_CARDS;
     }
 }
